a9ac8dacebf99a52583196a3b6b1d92bc0631ca6
Cybertoge <trejo.allan@yahoo.com>
2023-01-03 18:54:05 -0800

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key] //attribute that tells c# this is a primary key, type [key] and ctrl + . on key to open options and select using System 
+        public int Id { get; set; }
+        [Required] // sets Name as required
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
