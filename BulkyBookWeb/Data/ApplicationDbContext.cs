26f36c4dc1324f8ee647faa9ae75de00e01c60ea
Cybertoge <trejo.allan@yahoo.com>
2023-01-03 19:25:24 -0800

    Added database connection MS SQL

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBookWeb.Models; //imports Category model
+using Microsoft.EntityFrameworkCore; //install entity framework core do allow DbContext base class
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)  //Constructor that accepts 1 parameter
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; } // sets up a table with the defined model categories in category
+    }
+}
--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,8 +1,18 @@
+//THis file is similar to JS server file. all middleware is added to the App program
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. Here you add any middleware before the Builder.Build() function
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation(); //Is required to hotreload, add nuget runtimecompilation
+
+//This block of code is used to make a connection to the sql database defined with connection string in appsettings.json
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
 
 var app = builder.Build();
 
